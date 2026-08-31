807a96a3a2b66d0c577c526f9857949b5fa640f2
Zhixuan Jiang <alexjiang20232024@gmail.com>
2026-08-31 19:37:57 -0400

    fix(linear): expose catalog response within handler

--- server-rs/crates/patchbay-handler/src/linear.rs
+++ server-rs/crates/patchbay-handler/src/linear.rs
@@ -1155,7 +1155,7 @@ impl From<LinearCatalogLabel> for LinearCatalogLabelResponse {
 }
 
 #[derive(Debug, Serialize)]
-struct LinearCatalogResponse {
+pub(crate) struct LinearCatalogResponse {
     teams: Vec<LinearCatalogTeam>,
     projects: Vec<LinearCatalogProject>,
     states: Vec<LinearCatalogState>,
